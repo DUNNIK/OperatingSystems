5c050c69292c31c879f36659d8a2c9637c287a85
DUNNIK <dns.56@mail.ru>
2021-06-20 10:52:59 +0300

    add: all files from Operating System course

---
+++ lab5/newmem.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "" > report.log
+arr=()
+iter=0
+while true; do
+	arr+=( 1 2 3 4 5 6 7 8 9 10 )
+	let iter=iter+1
+	if [[ "${#arr[@]}" -ge "$1" ]]; then
+		echo The operation was successful
+		exit 0
+	fi
+done
